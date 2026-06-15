c0a1f1ad614f1a419f38d864b26b6fb7bf7936d3
elizzzz <ewize123@gmail.com>
2026-06-15 21:53:07 +0300

    Add Content Security Policy meta tag to donations page

--- resources/views/pages/donations.blade.php
+++ resources/views/pages/donations.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ __('Donate - Paw Forest') }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}?v={{ time() }}">
