7d59f52968cea17103d27281eca054d1a33afc6e
elizzzz <ewize123@gmail.com>
2026-06-15 21:53:44 +0300

    Update gallery.blade.php

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ __('Paw Forest - Gallery') }}</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('favicon.ico') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
 </head>
 <body>
